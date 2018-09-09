0d035ddd45b3c902d5d6ebcd29182dad1e8a5e1b
jestempablo <pawel@forynski.pl>
2018-09-09 18:14:39 +0200

    Mostly finished view part, imported normalize.css

--- src/DonateComponent/DonateComponent.js
+++ src/DonateComponent/DonateComponent.js
@@ -1,3 +1,47 @@
 import './DonateComponent.less';
 import template from './DonateComponent.html';
-console.log('DonateComponent loaded');
+
+(function DonateComponent() {
+
+    var init,
+        updateProgress,
+        giveNow,
+        validateInput,
+        collapseOpen,
+        collapseClose,
+        saveForLater;
+
+    init = function() {
+        var componentElement = document.createElement('div');
+        document.body.appendChild(componentElement);
+        componentElement.outerHTML = template;
+    }
+
+    updateProgress = function(){
+
+    }
+
+    giveNow = function(){
+
+    }
+
+    validateInput = function(){
+
+    }
+
+    collapseOpen = function(){
+
+    }
+
+    collapseClose = function(){
+
+    }
+
+    saveForLater = function(){
+
+    }
+
+    return init;
+
+    
+})()();
